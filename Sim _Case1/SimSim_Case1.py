a6ce40916a1bff835dade2e4a5e2dfbc260f68ff
Doogerr <71419642+Doogerr@users.noreply.github.com>
2025-05-01 12:31:29 +0800

    Add files via upload

---
+++ Sim _Case1/SimSim_Case1.py
@@ -0,0 +1,43 @@
+from Helper import Helpers
+from ArduinoPWM import ArduinoPWMController
+from Motor_PID import Motor
+from Cases_Data import Case1
+import numpy as np
+import time
+
+def awake():
+    print("Initializing hardware and parameters...")
+
+def main():
+    awake()
+    
+    controller = ArduinoPWMController('/dev/ttyACM0')
+    controller.connect()
+
+    case1 = Case1(2,4,6,5,5)
+
+    left_motor = Motor()
+    right_motor = Motor()
+
+    start_time = time.monotonic()
+
+    try:
+        while True:
+            current_time = time.monotonic()
+            pasted_time = current_time - start_time
+
+            v = case1.v(current_time)
+            w = case1.w(current_time)
+
+            left_motor.pwm, right_motor.pwm = Helpers.compute_pwm_from_velocity(v, w, 8.1, 15.2, 1)
+
+            controller.send_pwm(left_motor.pwm, right_motor.pwm)
+
+            # Simulate control loop delay
+            time.sleep(0.001)  # 1000 Hz
+
+    except KeyboardInterrupt:
+        print("Shutting down.")
+
+if __name__ == "__main__":
+    main()
