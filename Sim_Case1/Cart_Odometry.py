17ee6321dd323d196b2c219f2ae244c6336dff6f
Doogerr <71419642+Doogerr@users.noreply.github.com>
2025-05-07 17:57:40 +0800

    Update Cart_Odometry.py

--- Sim_Case1/Cart_Odometry.py
+++ Sim_Case1/Cart_Odometry.py
@@ -1,12 +1,12 @@
 import numpy as np
-from Helper import Helpers
+import math
 
 class CartOdometry:
-    def __init__(self, wheel_radius, wheel_base, last_left_deg: float, last_right_deg: float):
+    def __init__(self, wheel_radius, wheel_base, last_left_rad: float, last_right_rad: float):
         self.R = wheel_radius
         self.L = wheel_base
-        self.last_left_deg = last_left_deg
-        self.last_right_deg = last_right_deg
+        self.last_left_rad = last_left_rad
+        self.last_right_rad = last_right_rad
 
         # These are the decleration of position & angle, velocity & omega vectors
         self.pos = np.zeros(3)
@@ -14,48 +14,34 @@ class CartOdometry:
 
         self.input_state = "correct"
 
-    def update(self, encoder_data: np.array):
-        if len(encoder_data) > 4: self.input_state = "greater than 4"; return
-        if len(encoder_data) < 4: self.input_state = "less than 4"; return
-        if len(encoder_data) > 4: self.input_state = "currect"
+    def update(self, encoder_data: np.array, dt):
+        if len(encoder_data) > 2: self.input_state = "greater than 2"; return
+        if len(encoder_data) < 2: self.input_state = "less than 2"; return
+        if len(encoder_data) == 2: self.input_state = "currect"
 
         theta = self.pos[2]
 
         # Get encoder readings
-        left_deg = encoder_data[0]
-        right_deg = encoder_data[1]
-        left_omega = encoder_data[2]
-        right_omega = encoder_data[3]
+        left_rad = encoder_data[0]
+        right_rad = encoder_data[1]
 
-        d_left = (left_deg - self.last_left_deg) * self.R
-        d_right = (right_deg - self.last_right_deg) * self.R
-
-        left_vel = left_omega * self.R
-        right_vel = right_omega * self.R
+        d_left = (left_rad - self.last_left_rad) * self.R
+        d_right = (right_rad - self.last_right_rad) * self.R
+        d_avg = (d_right + d_left) / 2.0
+        dtheta = (d_right - d_left) / self.L
 
         # Estimate new position
-        '''
-        delta_pos = np.array([
-            ((d_left + d_right) * math.cos(theta)) / 2,
-            ((d_left + d_right) * math.sin(theta)) / 2,
-            (d_right - d_left) / (self.L * 2)
-        ])
-        '''
+        pos0 = self.pos[0] + (d_avg * math.cos(theta + dtheta / 2.0))
+        pos1 = self.pos[1] + (d_avg * math.sin(theta + dtheta / 2.0))
+        pos2 = dtheta
 
         # Estimate new velocity
-        '''
-        new_vel = np.array([
-            ((left_vel + right_vel) * math.cos(theta)) / 2
-            ((left_vel + right_vel) * math.sin(theta)) / 2
-            ((right_vel - left_vel) * self.R) / (self.L * 2)
-        ])
-        '''
+        self.vel[0] = (pos0 - self.pos[0]) / dt
+        self.vel[1] = (pos1 - self.pos[1]) / dt
+        self.vel[2] = (pos2 - self.pos[2]) / dt 
 
-        self.pos = self.pos + Helpers.ABphasetoPos(d_left, d_right, theta, self.L)
-        self.vel = Helpers.ABphasetoVel(left_vel, right_vel, theta, self.R, self.L)
-
-        self.last_left_deg = left_deg
-        self.last_right_deg = right_deg
+        self.last_left_rad = left_rad
+        self.last_right_rad = right_rad
 
 
     def get_pose(self):
@@ -65,4 +51,4 @@ class CartOdometry:
         return self.vel
 
     def reset(self):
-        self.__init__(self.left_encoder, self.right_encoder, self.R, self.L)
+        self.__init__(self.R, self.L, self.last_left_rad, self.last_right_rad)
